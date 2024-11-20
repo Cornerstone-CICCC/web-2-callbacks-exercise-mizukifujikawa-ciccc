caf6b905a2b90cc6b8edd9c5e53b3ec22acb2d8b
mizukifujikawa-ciccc <mz.fujikawa.ciccc@gmail.com>
2024-11-20 09:30:40 -0800

    exercise

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,12 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+  fs.readFile("./lastname.txt", (err, data2) => {
+    fs.readFile("./age.txt", (err, data3) => {
+      fs.readFile("./hobbies.txt", (err, data4) => {
+        const hobbies = JSON.parse(data4.toString()).join(" and ");
+        console.log(`${data1} ${data2} is ${data3} years old and his hobbies are ${hobbies}`);
+      });
+    });
+  });
 })
